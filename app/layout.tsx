ce7ed3bad55f832ccbe25a56e21ac2d3c5b31b8a
Julia Kleber <julia.kleber@gmx.net>
2024-03-21 12:44:20 +0100

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./globals.css";
+import Favicon from './favicon.ico';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Music Insights",
   description: "meta data about artists, albums and tracks",
+  icons: [{ rel: 'icon', url: Favicon.src }],
 };
 
 export default function RootLayout({
