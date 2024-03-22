e3c02ad1eda49d780a6ec7e6c19a12425d96dcb9
Julia Kleber <julia.kleber@gmx.net>
2024-03-22 10:19:22 +0100

    cleaned globals.css

--- app/stores/styleStore.ts
+++ app/stores/styleStore.ts
@@ -1,7 +1,7 @@
 export const cardStyle = "m-5 p-2 flex flex-col md:flex-row justify-between rounded-lg shadow-costum dark:bg-bgDarkCard";
 export const buttonStyle =
   "w-32 p-1 mt-4 mx-1 bg-sky-600 text-white rounded-md shadow-md border-none hover:bg-sky-800 hover:shadow-lg";
-export const linkStyle = "cursor-pointer dark:hover:text-pink-400 hover:text-primaryColor";
+export const linkStyle = "cursor-pointer dark:hover:text-pink-400 hover:text-red-600";
 export const arrowStyle = "text-red-300 dark:text-sky-300 cursor-pointer";
 export const firstColumnStyle = "font-bold text-right pr-2";
 export const secondColumnStyle = "pl-2";
